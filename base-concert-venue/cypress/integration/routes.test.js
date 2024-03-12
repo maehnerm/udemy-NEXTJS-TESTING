df385819abbada2406f81f3fb9f8a6c685128cde
Marcus Mähner <hey@marcusmaehner.com>
2024-03-12 23:24:58 +0100

    Making shure db-reset doesn't error out

---
+++ base-concert-venue/cypress/integration/routes.test.js
@@ -0,0 +1,17 @@
+it("shows the correct heading when navigating to shows route", () => {
+  cy.visit("/");
+  cy.findByRole("button", { name: /shows/i }).click();
+  cy.findByRole("heading", { name: /upcoming shows/i }).should("exist");
+});
+
+it("shows the correct heading when navigating to bands route", () => {
+  cy.visit("/");
+  cy.findByRole("button", { name: /bands/i }).click();
+  cy.findByRole("heading", { name: /our illustrious performers/i }).should(
+    "exist"
+  );
+});
+
+it("resets the database", () => {
+  cy.task("db:reset");
+});

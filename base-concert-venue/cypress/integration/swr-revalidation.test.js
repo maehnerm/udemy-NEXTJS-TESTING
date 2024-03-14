565a17f690c719ee8f68a917972ec79bc4ed142a
Marcus Mähner <hey@marcusmaehner.com>
2024-03-14 13:06:01 +0100

    Code quiz, interval updates

--- base-concert-venue/cypress/integration/swr-revalidation.test.js
+++ base-concert-venue/cypress/integration/swr-revalidation.test.js
@@ -3,6 +3,7 @@ import { generateRandomId } from "../../lib/features/reservations/utils";
 
 const ONE_SECOND = 1000;
 const THIRTY_SECONDS = 30 * ONE_SECOND;
+const FIFTEEN_SECONDS = 15 * ONE_SECOND;
 
 it("should refresh the shows page after 30 seconds", () => {
   cy.clock();
@@ -27,4 +28,33 @@ it("should refresh the shows page after 30 seconds", () => {
   // advance clock by 30 seconds; now additional show should display
   cy.tick(THIRTY_SECONDS);
   cy.findAllByText(/sold out/i).should("have.length", 2);
+});
+
+it("should refresh the reservation page after 15 seconds", () => {
+  cy.clock();
+  cy.task("db:reset").visit("/reservations/0");
+
+  cy.findByRole("main").within(() =>
+    cy.findByRole("button", { name: /sign in/i }).click()
+  );
+
+  // there should be ten seats available for reservation
+  cy.findByText(/10 seats left/i).should("exist");
+
+  // buy 2 tickets from the show (id 0, 10 seats available)
+  const newReservation = generateNewReservation({
+    reservationId: generateRandomId(),
+    showId: 0,
+    seatCount: 2,
+  });
+
+  cy.task("addReservation", newReservation);
+
+  // advance the clock by 1 second; there should still be 10 seats available
+  cy.tick(ONE_SECOND);
+  cy.findByText(/10 seats left/i).should("exist");
+
+  // advance the clock by 15 seconds; there should only be 8 seats available
+  cy.tick(FIFTEEN_SECONDS);
+  cy.findByText(/8 seats left/i).should("exist");
 });

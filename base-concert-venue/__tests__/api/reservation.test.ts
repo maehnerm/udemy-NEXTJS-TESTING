c175393fceb5f6e58291dc2332bbd507e3f33905
Marcus Mähner <hey@marcusmaehner.com>
2024-03-16 00:16:53 +0100

    Disabling mocking function

--- base-concert-venue/__tests__/api/reservation.test.ts
+++ base-concert-venue/__tests__/api/reservation.test.ts
@@ -1,9 +1,11 @@
 import { testApiHandler } from "next-test-api-route-handler";
 
+import { validateToken } from "@/lib/auth/utils";
 import reservationsHandler from "@/pages/api/reservations/[reservationId]";
 import usersReservationsHandler from "@/pages/api/users/[userId]/reservations";
 
 jest.mock("@/lib/auth/utils");
+const mockedValidateToken = validateToken as jest.Mock;
 
 test("POST /api/reservations creates a new reservation assosiated with the user", async () => {
   await testApiHandler({
@@ -37,6 +39,30 @@ test("POST /api/reservations creates a new reservation assosiated with the user"
       const json = await res.json();
 
       expect(json.userReservations).toHaveLength(3);
+    },
+  });
+});
+
+test("POST /api/reservations/[reservationID] returns 401 status when not authorized", async () => {
+  mockedValidateToken.mockResolvedValue(false);
+
+  await testApiHandler({
+    handler: reservationsHandler,
+    paramsPatcher: (params) => {
+      // eslint-disable-next-line no-param-reassign
+      params.reservationId = 1234;
+    },
+    test: async ({ fetch }) => {
+      const res = await fetch({
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ seatCount: 2, userId: 1, showId: 0 }),
+      });
+
+      // expect unauthorized
+      expect(res.status).toBe(401);
     },
   });
 });
--- base-concert-venue/__tests__/api/user.test.ts
+++ base-concert-venue/__tests__/api/user.test.ts
@@ -1,9 +1,11 @@
 import { testApiHandler } from "next-test-api-route-handler";
 
+import { validateToken } from "@/lib/auth/utils";
 import usersReservationsHandler from "@/pages/api/users/[userId]/reservations";
 import userAuthHandler from "@/pages/api/users/index";
 
 jest.mock("@/lib/auth/utils");
+const mockedValidateToken = validateToken as jest.Mock;
 
 test("POST /api/users receives token with correct credentials", async () => {
   await testApiHandler({
@@ -59,6 +61,23 @@ test("GET /api/user/[userID]/reservation returns empty array for user without an
       expect(res.status).toBe(200);
       const json = await res.json();
       expect(json.userReservations).toHaveLength(0);
+    },
+  });
+});
+
+test("GET /api/user/[userID]/reservation returns 401 status when unauthorized", async () => {
+  mockedValidateToken.mockResolvedValue(false);
+  await testApiHandler({
+    handler: usersReservationsHandler,
+    paramsPatcher: (params) => {
+      // eslint-disable-next-line no-param-reassign
+      params.userId = 1;
+    },
+    test: async ({ fetch }) => {
+      const res = await fetch({
+        method: "GET",
+      });
+      expect(res.status).toBe(401);
     },
   });
 });

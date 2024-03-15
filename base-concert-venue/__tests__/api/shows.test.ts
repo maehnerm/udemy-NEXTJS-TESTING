4ba2b36bdfe16ebafbf0a43e61e8d8f3cf2c804a
Marcus Mähner <hey@marcusmaehner.com>
2024-03-15 12:15:41 +0100

    Testing a dynamic GET route

--- base-concert-venue/__tests__/api/shows.test.ts
+++ base-concert-venue/__tests__/api/shows.test.ts
@@ -3,8 +3,9 @@ import { testApiHandler } from "next-test-api-route-handler";
 import { readFakeData } from "@/__tests__/__mocks__/fakeData";
 // Import the handler under test from the pages/api directory
 import showsHandler from "@/pages/api/shows/index";
+import showIdHandler from "@/pages/api/shows/[showId]";
 
-test("/api/shows returns shows from database", async () => {
+test("GET /api/shows returns shows from database", async () => {
   await testApiHandler({
     handler: showsHandler,
     test: async ({ fetch }) => {
@@ -15,6 +16,24 @@ test("/api/shows returns shows from database", async () => {
       const { fakeShows } = await readFakeData();
 
       expect(json).toEqual({ shows: fakeShows });
+    },
+  });
+});
+
+test("GET /api/shows/[showId] returns the data for the correct show ID", async () => {
+  await testApiHandler({
+    handler: showIdHandler,
+    paramsPatcher: (params) => {
+      // eslint-disable-next-line no-param-reassign
+      params.showId = 0;
+    },
+    test: async ({ fetch }) => {
+      const res = await fetch({ method: "GET" });
+      expect(res.status).toBe(200);
+      const json = await res.json();
+      const { fakeShows } = await readFakeData();
+
+      expect(json).toEqual({ show: fakeShows[0] });
     },
   });
 });

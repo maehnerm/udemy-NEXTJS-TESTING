24f9fcf36957ad9d937954d28da142aa8a642e7c
Marcus Mähner <hey@marcusmaehner.com>
2024-03-15 11:36:16 +0100

    First api test

--- base-concert-venue/jest.setup.js
+++ base-concert-venue/jest.setup.js
@@ -5,8 +5,22 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
 
+// polyfill necessary for jsdom test environment
+// reference: https://www.stackoverflow.com/a/68468204
+import { TextDecoder, TextEncoder } from "util";
+
+import { resetDB } from "@/__tests__/__mocks__/db/utils/reset-db";
 import { server } from "@/__tests__/__mocks__/msw/server";
 
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
 beforeAll(() => server.listen());
+
+beforeEach(async () => {
+  await resetDB();
+});
+
 afterEach(() => server.resetHandlers());
+
 afterAll(() => server.close());
